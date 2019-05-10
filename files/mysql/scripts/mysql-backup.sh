63d9850268b54ad06f37defeaba1a9bb9e4abb70
chen wenhao <wenhao.chen@anlaa.com>
2019-05-10 11:13:32 +0800

    x

--- files/mysql/scripts/mysql-backup.sh
+++ files/mysql/scripts/mysql-backup.sh
@@ -2,4 +2,4 @@
 
 zDATE=\$(date +%Y%m%d)
 mkdir /data/mysql/backup/\$zcDATE
-mysqldump -h '127.0.0.1' -uroot -p'123456' --databases zphaldb > /data/mysql/backup/\$zDATE/zphaldb.sql
+mysqldump -h '127.0.0.1' -uroot -p'vegeta1617' --databases dboadmin > /data/mysql/backup/\$zDATE/dboadmin.sql
